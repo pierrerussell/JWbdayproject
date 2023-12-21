6a1bcf1d13209a126a4383b9981c9ab7c4474592
pierrerussell <63531776+pierrerussell@users.noreply.github.com>
2023-12-21 09:54:34 +0800

    Update views.py

--- jaewonsbday/views.py
+++ jaewonsbday/views.py
@@ -12,7 +12,7 @@ def index(request):
     else:
         isbday = False
 
-    timeleft = datetime.date(date.today().year, 12, 2) - datetime.date.today
+    timeleft = datetime.date(date.today().year, 12, 2) - datetime.date.today()
 
     return render(request, 'jaewonsbday/index.html', {
         'isbday': isbday,
